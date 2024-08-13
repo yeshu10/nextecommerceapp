dfa35f493ba43a4813071c0f8d0de27a612ae25c
yeshu10 <yeshvigupta10@gmail.com>
2024-08-14 00:33:09 +0530

    Header added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,15 @@
 // src/pages/_app.js
-import '../styles/globals.css'; // Ensure global styles are imported
+import '../styles/globals.css'; // Import global styles if you have any
+import { CartProvider } from '../context/CartContext'; // Adjust the path based on your project structure
+import Header from '@/components/Header';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <CartProvider>
+        <Header/>
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
+
+export default MyApp;
